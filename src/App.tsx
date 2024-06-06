8fd82f39cf7e8d2d6f8b064373bfcc9dfba9449d
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-06-06 19:13:51 -0300

    Primeiras mudanças

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import EstiloGlobal, { Container } from './styles'
+import Cores from './themes/temacores'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={Cores}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/themes/temacores.ts
@@ -0,0 +1,6 @@
+const Cores = {
+  corPrimaria: 'a7727d',
+  corSegundaria: 'f9f5e7'
+}
+
+export default Cores
